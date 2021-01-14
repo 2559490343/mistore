7f21dc440ee8705d334c4042d97070e9cc4d548a
2559490343 <2559490343@qq.com>
2021-01-14 22:29:31 +0800

    xyc

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,51 +3,41 @@
 const app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    goodList: [
+      {
+        img: '../../static/img/phone1.jpg',
+        name: 'Redmi K30 4G',
+        text: '120Hz流速屏，全速热爱',
+        price: '￥1299起'
+      },
+      {
+        img: '../../static/img/phone2.jpg',
+        name: 'Redmi K30 5G',
+        text: '双模5G，120Hz流速屏',
+        price: '￥1599起'
+      },
+      {
+        img: '../../static/img/phone3.jpg',
+        name: 'Note 8 Pro',
+        text: '6400万全场景四摄',
+        price: '￥1099起'
+      },
+      {
+        img: '../../static/img/phone4.jpg',
+        name: 'Redmi Note 8',
+        text: '千元4800万四摄',
+        price: '￥899起'
+      },
+    ]
   },
   // 事件处理函数
-  bindViewTap() {
+  toDetail(e) {
+    const target = e.target;
+    const index = target.dataset.index;
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../detail/detail?index=' + index
     })
   },
   onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
