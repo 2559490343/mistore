6dd9fcc245aa30b8b4fc2ad47cf61f3d25f939bb
2559490343 <2559490343@qq.com>
2021-01-15 18:53:58 +0800

    xyc

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -7,7 +7,16 @@ Page({
   data: {
 
   },
-
+  toLogin(){
+    wx.navigateTo({
+      url: '../login/login',
+    })
+  },
+  toCar(){
+    wx.switchTab({
+      url: '../car/car',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
